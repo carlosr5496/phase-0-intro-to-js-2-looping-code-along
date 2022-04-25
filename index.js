54308e5442d1621ba429ac494aa8413fb678d483
Ryan Carlos <ryancarlos925@gmail.com>
2022-04-25 08:27:04 -0700

    commit tree f9ca846ff0c0397ee3eb16cda46999cec82c6d28

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names,event) {
+    for (let i = 0; i < 3; i++) {
+    return [`Thank you, ${names[0]}, for the wonderful ${event} gift!`,
+    `Thank you, ${names[1]}, for the wonderful ${event} gift!`,
+    `Thank you, ${names[2]}, for the wonderful ${event} gift!`];
+    }
+}
+
+function countDown() { 
+    let i = 10;  
+    while ( i >= 0) {
+        console.log (i);
+        i--;
+    }
+}
